d7918095bde3cd99fb7d8a7675fc2a533b43b2ff
Darius346 <62722496+Darius346@users.noreply.github.com>
2020-07-09 22:48:43 -0400

    this update allows users to search albums by their unique id

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /music/
+    # url(r'^$', views.index, name='index'),
     path('', views.index, name='index'),
+
+    # /music/71/
+    # url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail),
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,10 @@
-#views are python functions that takes user requests and return results
-#  
+#views are python functions that takes user requests and 
+#returns requests/html.
 
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>This is my Music app homepage!</h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
+    
