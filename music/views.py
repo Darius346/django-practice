2c10cd970b99ff499f3a9ac0971cea7891ea9d88
Darius346 <62722496+Darius346@users.noreply.github.com>
2020-07-10 00:31:04 -0400

    on the music page you will see albums available

--- music/views.py
+++ music/views.py
@@ -2,9 +2,17 @@
 #returns requests/html.
 
 from django.http import HttpResponse
+from .models import Album
+
+#all_albums will connect to database and get all objects available and
+#display results on the music page
 def index(request):
-    return HttpResponse("<h1>This is my Music app homepage!</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        path = '/music/' + str(album.id) + '/'
+        html += '<a href="' + path + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
-    
