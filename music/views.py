c2b24e23efbb8f8f52a9d1f5ac57bc1f0edb843f
Darius346 <62722496+Darius346@users.noreply.github.com>
2020-07-15 16:22:29 -0400

    templates

--- music/views.py
+++ music/views.py
@@ -2,17 +2,18 @@
 #returns requests/html.
 
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 #all_albums will connect to database and get all objects available and
 #display results on the music page
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        path = '/music/' + str(album.id) + '/'
-        html += '<a href="' + path + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context= {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
