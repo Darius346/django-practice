9b5a88bc7392fef3570bca653a9dc950e553de33
Darius346 <62722496+Darius346@users.noreply.github.com>
2020-07-22 21:01:26 -0400

    added function to favorite button

--- music/views.py
+++ music/views.py
@@ -2,7 +2,7 @@
 #returns requests/html.
 
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 
 #all_albums will connect to database and get all objects available and
@@ -17,4 +17,14 @@ def detail(request, album_id):
 
 def favorite(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/favorite.html', {'album': album})
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+        'album': album,
+        'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
